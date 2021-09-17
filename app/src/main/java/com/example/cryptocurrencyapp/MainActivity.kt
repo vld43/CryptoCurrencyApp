13072dcea13c7a1b84b9fb5972465f9c891e7972
vova4 <435335vova@gmail.com>
2021-09-17 21:29:40 +0300

    Создание рекламных баннеров.

--- app/src/main/java/com/example/cryptocurrencyapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/MainActivity.kt
@@ -3,15 +3,30 @@ package com.example.cryptocurrencyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.example.cryptocurrencyapp.activities.AboutActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var interstitialAd: InterstitialAd
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        interstitialAd = InterstitialAd(this)
+        interstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        interstitialAd.loadAd(AdRequest.Builder().build())
+
+//        val str = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
+//
+//        Log.i("qq", "$str")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,11 +37,22 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_about -> {
-                val intent = Intent(this, AboutActivity::class.java)
-                startActivity(intent)
+                startActivity(Intent(this, AboutActivity::class.java))
                 return true
             }
         }
+
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        showAd()
+    }
+
+    private fun showAd() {
+        if(interstitialAd.isLoaded) {
+            interstitialAd.show()
+        }
     }
 }
--- app/src/main/java/com/example/cryptocurrencyapp/activities/AboutActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/activities/AboutActivity.kt
@@ -4,18 +4,26 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import com.example.cryptocurrencyapp.R
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 
 class AboutActivity : AppCompatActivity() {
 
     private val buttonRateApp by lazy { findViewById<Button>(R.id.button_rate_app) }
+    private val adView by lazy { findViewById<AdView>(R.id.ad_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val adRequest = AdRequest.Builder().build()
+
+        adView.loadAd(adRequest)
 
         buttonRateApp.setOnClickListener {
             startActivity(
